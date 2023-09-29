721a333ec3350ae3e1a6c90f542656aad6c8bed8
quilicicf <quilicicf@gmail.com>
2023-09-29 16:53:56 +0200

    :up: Add support for line-numbers plugin for code highlight

--- index.mjs
+++ index.mjs
@@ -18,7 +18,14 @@ const BUILD_AREA_PATH = resolve(REPOSITORY_ROOT_PATH, 'build-area');
 const DIST_FOLDER_PATH = resolve(REPOSITORY_ROOT_PATH, 'dist');
 const NODE_MODULES_PATH = resolve(REPOSITORY_ROOT_PATH, 'node_modules');
 const PRISM_CSS_PATH = resolve(NODE_MODULES_PATH, 'prismjs', 'themes', 'prism-tomorrow.css');
-// const PRISM_LINE_NUMBERS__CSS_PATH = resolve(NODE_MODULES_PATH, 'prismjs', 'plugins', 'line-numbers', 'prism-line-numbers.css');
+
+const PRISM_PLUGINS = {
+  'line-numbers': {
+    cssPath: resolve(NODE_MODULES_PATH, 'prismjs', 'plugins', 'line-numbers', 'prism-line-numbers.css'),
+    additionalCss: `pre.highlight.line-numbers code { overflow: unset; }`,
+    pluginPath: 'prismjs/plugins/line-numbers/prism-line-numbers.js',
+  },
+};
 
 const BUILT_DECK_JS_FILE_PATH = resolve(BUILD_AREA_PATH, 'deck.js');
 const BUILT_DECK_CSS_FILE_PATH = resolve(BUILD_AREA_PATH, 'deck.css');
@@ -84,26 +91,37 @@ async function highlightCode (dom) {
 
   console.info(`Loading languages: [ ${languages.join(', ')} ]`);
   loadLanguages(languages);
-  const preparedDom = prepareHighlighting(dom);
 
-  // global.window = dom.window; // NOTE: required for Prism plugins, emulates a browser environment
-  // global.document = dom.window.document; // NOTE: required for Prism plugins, emulates a browser environment
-  // global.getComputedStyle = window.getComputedStyle; // Line-numbers plugin uses it as if in a browser => window instead of global
-  // await import('prismjs/plugins/line-numbers/prism-line-numbers.js'); // FIXME: for some reason, the line number don't show up
-  // await import('prismjs/plugins/keep-markup/prism-keep-markup.js'); // FIXME: needs manual Asciidoctor parsing, or it escapes markup in code blocks
+  const pluginsToActivate = Object.entries(PRISM_PLUGINS)
+    .filter(([ key ]) => $$(dom, `.${key}`).length)
+    .map(([ , plugin ]) => plugin);
 
+  const preparedDom = await prepareHighlighting(dom, pluginsToActivate);
   Prism.highlightAllUnder(preparedDom.window.document); // NOTE: MUTATES AST!
   return preparedDom;
 }
 
-function prepareHighlighting (dom) {
+async function prepareHighlighting (dom, pluginsToActivate) {
+  global.window = dom.window; // NOTE: required for Prism plugins, emulates a browser environment
+  global.document = dom.window.document; // NOTE: required for Prism plugins, emulates a browser environment
+  global.getComputedStyle = window.getComputedStyle; // Line-numbers plugin uses it as if in a browser => window instead of global
+
   // $$(dom, 'code')
   //   .forEach((element) => element.children.length = element?.childNodes?.length); // FIXME: Keep markup plugin expects that
 
+  const pluginsCss = await pluginsToActivate
+    .reduce(
+      (promise, plugin) => promise.then(async (seed) => {
+        await import(plugin.pluginPath);
+        const pluginCss = readFileSync(plugin.cssPath, 'utf8');
+        return `${seed}${pluginCss}${plugin.additionalCss}`;
+      }),
+      Promise.resolve(''),
+    );
+
   const prismCss = readFileSync(PRISM_CSS_PATH);
-  // const lineNumbersCss = readFileSync(PRISM_LINE_NUMBERS__CSS_PATH); // FIXME: re-add and make optional depending on whether it's used
   const headNode = $(dom, 'head');
-  headNode.insertAdjacentHTML('beforeend', `<style id="CSS_PRISM">${prismCss}</style>`);
+  headNode.insertAdjacentHTML('beforeend', `<style id="CSS_PRISM">${prismCss}${pluginsCss}</style>`);
   return dom;
 }
 
